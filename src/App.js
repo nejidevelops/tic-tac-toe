42c55d816dbb58763fbcd79b8bfba1876770ea73
Newton Ombese <newtonombese1@gmail.com>
2023-03-22 09:14:35 +0300

    :art::boom:feat: Introducing the `useState` hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 
-function Square({ value }){
+function Square(){
+    const [value, setValue] = useState(null);
+
     function handleClick(){
-        console.log('clicked!');
+        setValue('X');
     }
     return(
         <button 
@@ -18,19 +21,19 @@ export default function Board(){
     return(
         <>
             <div className="board-row">
-                <Square value="1" />
-                <Square value="2" />
-                <Square value="3" />
+                <Square />
+                <Square />
+                <Square />
             </div>
             <div className="board-row">
-                <Square value="4" />
-                <Square value="5" />
-                <Square value="6" />
+                <Square />
+                <Square />
+                <Square />
             </div>
             <div className="board-row">
-                <Square value="7" />
-                <Square value="8" />
-                <Square value="9" />
+                <Square />
+                <Square />
+                <Square />
             </div>
         </>
     )
